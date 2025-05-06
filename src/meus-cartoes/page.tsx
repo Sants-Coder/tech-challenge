85796614ebf2affd22f08a076bfbb995dcd23589
Henrique <ha701430@gmail.com>
2025-05-05 23:16:13 -0300

    feat(pages): adiciona páginas de Serviços e Meus Cartões

---
+++ src/meus-cartoes/page.tsx
@@ -0,0 +1,9 @@
+export default function MeusCartoesPage() {
+    return (
+      <main>
+        <h1>Meus Cartões</h1>
+        <p>Meus Cartões.</p>
+      </main>
+    );
+  }
+  
---
+++ src/servicos/page.tsx
@@ -0,0 +1,9 @@
+export default function ServicosPage() {
+    return (
+      <main>
+        <h1>Serviços</h1>
+        <p>Pagina de serviços.</p>
+      </main>
+    );
+  }
+  
